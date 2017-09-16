2bb36a97ca6bd11695b5dc03ba9ef3451764fbbe
Fabio <islerfab@gmail.com>
2017-09-16 14:57:53 +0200

    removed unused comments

--- local.sample.js
+++ local.sample.js
@@ -1,10 +1,3 @@
-// Use local.env.js for environment variables that grunt will set when the server starts locally.
-// Use for your api keys, secrets, etc. This file should not be tracked by git.
-//
-// You will need to set these on the server you deploy to.
-
-// Create a local.env.js with the proper content!
-
 module.exports = {
     consumer_key: 'PUT',
     consumer_secret: 'STUFF',
