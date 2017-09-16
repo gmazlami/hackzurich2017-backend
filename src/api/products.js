4cfc38a12e7b17ffc3acdccedf208a961d90e99c
Dominik Schöni <dominik@dominikschoeni.ch>
2017-09-16 19:24:12 +0200

    refactoring

--- src/api/products.js
+++ src/api/products.js
@@ -1,5 +1,5 @@
 const request = require('request');
-
+const InsuranceService = require('../services/insurance');
 const DEMO_PRODUCT_EAN = "5030930121822";
 const DEMO_PRODUCT_SENTIMENT = "0.7";
 
@@ -10,24 +10,25 @@ const generateRandomSentiment = () => {
 }
 
 const enrichProduct = (product) => {
-  if (product["ean"] == DEMO_PRODUCT_EAN) {
-    product["sentiment"] = DEMO_PRODUCT_SENTIMENT;
+  if (product["ean"] === DEMO_PRODUCT_EAN) {
+    product.sentiment = DEMO_PRODUCT_SENTIMENT;
   } else {
-    product["sentiment"] = generateRandomSentiment();
+    product.sentiment = generateRandomSentiment();
   }
+
+  product.insurancePrice = InsuranceService.computeInsurancePrice(product.price, product.sentiment)
 
   return product;
 }
 
 exports.get = (req, res) => {
-
   request('https://api.siroop.ch/product/concretes/sku/' + req.params.id + '?apikey=8ccd66bb1265472cbf8bed4458af4b07', { json: true }, (err, result, body) => {
     if (err) {
       console.log(err);
       return res.status(500);
     }
 
-    let product = enrichProduct(body)
+    let product = enrichProduct(body[0])
     return res.status(200).json(product)
   });
 }
